5216472bd5bfc0f7574d1f050c70a7be61154555
Urvashi-Patel99 <urvashipatel87935@gmail.com>
2025-03-26 13:49:33 +0530

    Add files via upload

---
+++ Practice4.php
@@ -0,0 +1,40 @@
+<?php
+ //write a php function countOccurrences that takes two array and return an assiciative array where the key are the unique values from the input array and the values are their counts.
+
+ function countOccurrences(){
+ 
+ $arr2 = ["Urvashi","Anjali","Mahi","Princi","Diksha","Sanjana","Urvashi","Anjali","Mahi","Princi"];
+ $newArray = array_count_values($arr2);
+
+ echo "<pre>";
+ print_r($newArray);
+ echo"</pre>";
+ }
+
+ countOccurrences();
+
+
+ //write a php function removeduplicate that takes an array and returns a new array with duplicate values removed.
+
+ function removeDuplicate(){
+  $arr2 = ["Urvashi","Anjali","Mahi","Princi","Diksha","Sanjana","Urvashi","Anjali","Mahi","Princi"];
+  $newArray = array_unique($arr2);
+  echo "<pre>";
+ print_r($newArray);
+ echo"</pre>";
+
+ }
+ removeDuplicate();
+
+ //write a php function reversearray that takes an array and returns a new array with the elements in reverse order.
+
+ function reverseArray(){
+  $arr2 = ["Urvashi","Anjali","Mahi","Princi","Diksha","Sanjana"];
+
+  $newArray = array_reverse($arr2);
+  echo "<pre>";
+ print_r($newArray);
+ echo"</pre>";
+ }
+ reverseArray();
+?>
